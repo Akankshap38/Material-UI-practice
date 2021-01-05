9278a13b896f4109d3cc7194a2c88aaf8cb22aa8
Akankshap38 <75260084+Akankshap38@users.noreply.github.com>
2021-01-05 19:05:30 +0530

    useStyle hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "./styles.css";
-import { Button } from "@material-ui/core";
+import { Button, makeStyles, Typography } from "@material-ui/core";
 
+const useStyles = makeStyles({
+  myStyle: {
+    backgroundColor: "yellow",
+    fontStyle: "oblique",
+    color: "brown",
+    fontWeight: "bolder"
+  }
+});
 export default function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <Button variant="contained" color="primary" size="medium" fullWidth>
         Click me
       </Button>
-
+      <Typography variant="h3" color="secondary">
+        Hello World
+      </Typography>
       <Button
         style={{ marginTop: "10px" }}
         variant="outlined"
@@ -17,6 +28,8 @@ export default function App() {
       >
         Click me
       </Button>
+
+      <Typography className={classes.myStyle}>Make style hook</Typography>
     </div>
   );
 }
